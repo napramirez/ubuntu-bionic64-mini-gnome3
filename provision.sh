55b0119f89561fe4d73dba382286f402bb5d2f37
Nap Ramirez <napramirez@gmail.com>
2017-03-29 10:17:25 +0000

    Reorder the configuration of lightdm

--- provision.sh
+++ provision.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 
-apt-get update
-apt-get install -y --no-install-recommends ubuntu-desktop gnome-keyring gedit gnome-terminal indicator-session unity-lens-applications
 echo "
 [SeatDefaults]
 autologin-user=ubuntu
 " > /etc/lightdm/lightdm.conf.d/autologin.conf
+
+apt-get update
+apt-get install -y --no-install-recommends ubuntu-desktop gnome-keyring gedit gnome-terminal indicator-session unity-lens-applications
 
 echo "ubuntu:ubuntu" | chpasswd
 
